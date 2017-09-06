932b086ae0a10b20b2af794d8fa4353e16ab9364
Jesse Clark <jesse@clarkdeveloper.com>
2017-09-06 13:07:19 -0500

    Update Upload to lambda

--- upload.py
+++ upload.py
@@ -4,18 +4,20 @@ import StringIO
 import zipfile
 import mimetypes
 
-
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-
-dist_bucket = s3.Bucket('portfolio.clarkdeveloper.com')
-src_bucket = s3.Bucket('portfoliobuild.clarkdeveloper.com')
-
-build_zip = StringIO.StringIO()
-src_bucket.download_fileobj('portfolioBuild.zip', build_zip)
-
-with zipfile.ZipFile(build_zip) as tmpZip:
-    for name in tmpZip.namelist():
-        obj = tmpZip.open(name)
-        dist_bucket.upload_fileobj(obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-        dist_bucket.Object(name).Acl().put(ACL='public-read')
-            
+def lambda_handler(event, context):
+    
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    
+    dist_bucket = s3.Bucket('portfolio.clarkdeveloper.com')
+    src_bucket = s3.Bucket('portfoliobuild.clarkdeveloper.com')
+    
+    build_zip = StringIO.StringIO()
+    src_bucket.download_fileobj('portfolioBuild.zip', build_zip)
+    
+    with zipfile.ZipFile(build_zip) as tmpZip:
+        for name in tmpZip.namelist():
+            obj = tmpZip.open(name)
+            dist_bucket.upload_fileobj(obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            dist_bucket.Object(name).Acl().put(ACL='public-read')
+    print 'Job Done!'
+    return 'Build Complete'
