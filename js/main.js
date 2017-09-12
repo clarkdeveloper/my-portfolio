0eeef2afa3e47d38da2a4a8061082b1d555d798b
Jesse Clark <jesse@clarkdeveloper.com>
2017-09-12 10:02:24 -0500

    add js/example-work.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-import 'react-dom'
-
-console.log("Loaded react-dom");
-
-console.log("Done Loading Webpack");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
+const myWork = [
+    {
+        'title': "Inspection App",
+        'image': {
+            'desc': "Login Screen",
+            'src': "images/eigi.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Portfolio",
+        'image': {
+            'desc': "Portfolio",
+            'src': "images/portfolio.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "AWS CI\CD",
+        'image': {
+            'desc': "Pipleline for AWS CI\CD",
+            'src': "images/aws.png",
+            'comment': ""
+        }
+    }
+]
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
